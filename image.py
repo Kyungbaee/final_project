d7e2d441f5b85bb5a948bd39e2b722d57058e985
kyungbae <tkadk2005@likelion.org>
2022-08-18 21:23:27 +0900

    fix

--- image.py
+++ image.py
@@ -2,13 +2,10 @@ import streamlit as st
 st.set_page_config(layout="wide")
 import torch
 import torchvision
-# import torchvision.datasets as datasets # 이미지 데이터셋 집합체
-# from torchvision import transforms # 이미지 데이터 transform
-# import torchvision.transforms as transforms # 이미지 변환 툴
 from torchvision import models
 import torch.nn as nn
 
-from torch.utils.data import DataLoader # 학습 및 배치로 모델에 넣어주기 위한 툴
+from torch.utils.data import DataLoader 
 from torch.utils.data import DataLoader, Dataset
 import albumentations
 import albumentations.pytorch
@@ -18,19 +15,13 @@ from PIL import Image
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-# @st.cache
 def load_model():
     
-    # 전체 모델을 통째로 불러옴, 클래스 선언 필수
     model = models.resnet50(pretrained=True).to(device)
     model.fc = nn.Linear(model.fc.in_features, 3).to(device)
     state_dict = torch.load(r'C:\Users\pc\Desktop\streamlit\final_project\weights\best_model_weight_8_17.pt')
-    # for k, v in state_dict['net'].items():
-    #     state_dict['net'][k] = v.cpu()
-    
+
     model.load_state_dict(state_dict['net'])
-    # model.load_state_dict(torch.load(r'C:\Users\pc\Desktop\streamlit\final_project\weights\model_state_dict.pt'))  # state_dict를 불러 온 후, 모델에 저장
-    # model.eval()
     return model
 
 
@@ -43,26 +34,21 @@ albumentations_test = albumentations.Compose([
 ])
 
 class CustomDataset(Dataset):
-    def __init__(self, img_path, label, train_mode=True, transforms=None): #필요한 변수들을 선언
+    def __init__(self, img_path, label, train_mode=True, transforms=None): 
         self.transforms = transforms
         self.train_mode = train_mode
         self.img_path = img_path
         self.label = label
         
 
-    def __getitem__(self, index): #index번째 data를 return
+    def __getitem__(self, index): 
         img_path = self.img_path
         index = 0
-        # img = Image.open(img_path)
-        # img = img.convert('RGB')
-        # image = img.resize((128, 128))
+
 
         image = cv2.imread(img_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # BGR -> RGB 변환
-        # img = load_image(img_path)
-        # image = cv2.imread(img)
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # BGR -> RGB 변환
-        # st.write(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+
 
         if self.transforms is not None:
             augmented = self.transforms(image=image)
@@ -74,10 +60,10 @@ class CustomDataset(Dataset):
         else:
             return image
     
-    def __len__(self): #길이 return
+    def __len__(self): 
         return len(self.img_path)
 
-# @st.cache
+
 def test(model, vali_loader):
     model.eval()
     with torch.no_grad():
@@ -116,12 +102,6 @@ if image_file is not None:
 
 if image_file is not None:
 
-	# To See details
-	# file_details = {"filename":image_file.name, "filetype":image_file.type,
-    #                           "filesize":image_file.size}
-	# st.write(file_details)
-    
-    # To View Uploaded Image
 	st.image(load_image(image_file),width=250)
     
 
